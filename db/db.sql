ddffefb81ca33bfbb931a9f9208969490e5ae3c7
Brad Kuhne <bjkuhne@aol.com>
2022-02-12 11:46:15 -0600

    setup employeemgr db and employee table and initial schemas and seed files.  Still need to setup gitignor file nodjs etc

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS employeemgr;
+CREATE DATABASE employeemgr;
+USE employeemgr;
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Micahel', 'Jordan', 1, 1),
+  ('Scotty', 'Pippen', 2, 2),
+  ('Charles', 'Barkley', 3, 3),
+  ('Larry', 'Bird', 4, 4),
+  ('Julius', 'Irving', 5, 5),
+  ('Magic', 'Johnson', 1, 2),
+  ('Patrick', 'Ewing', 2, 3),
+  ('Larry', 'Johnson', 3, 4),
+  ('Kareem', 'Abdul-Jabar', 4, 5),
+  ('Spud', 'Webb', 1, 1);
